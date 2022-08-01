6efb03a9c66cb027f5edd722fd2d5c987b51a3b3
ayment <93206027+ayment@users.noreply.github.com>
2022-08-01 03:07:06 +0300

    Update AssemblyInfo.cs

--- dll injector/dll injector test/Properties/AssemblyInfo.cs
+++ dll injector/dll injector test/Properties/AssemblyInfo.cs
@@ -19,8 +19,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("82cdcd3a-b6e9-42d1-aba9-404615338925")]
 
 // Version information for an assembly consists of the following four values:
 //
